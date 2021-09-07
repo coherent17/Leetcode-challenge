9de066c56ebbf110326a98d0f9c308abeca5a2e0
coherent17 <mnb51817@gmail.com>
2021-09-07 13:30:53 +0800

    hash table linear probing

--- 1. Two Sum/Two_Sum-hash_table-linear_probing.c
+++ 1. Two Sum/Two_Sum-hash_table-linear_probing.c
@@ -4,90 +4,98 @@
 #define TABLE_SIZE 20000
 #define EMPTY -1000000001
 
-int hash_table[TABLE_SIZE];
+typedef struct{
+    int value;
+    int index;
+} table;
 
-
-void init_hash_table(int *hash_table){
+void init_hash_table(table *hash_table){
     for (int i = 0; i < TABLE_SIZE;i++){
-        hash_table[i] = EMPTY;
+        hash_table[i].value = EMPTY;
     }
 }
 
-//hash function
-int hash(int number){
-    return (number > 0) ? (number * 2617) % TABLE_SIZE : (-number * 2617) % TABLE_SIZE;
+int hash(int value){
+    value = value > 0 ? value : -value;
+    return (value * 3) % TABLE_SIZE;
 }
 
-//insert the number into hash table
-void hash_table_insert(int number, int *hash_table){
-    //calculate the index by hash function
-    int index = hash(number);
-
-    //find the open address in hash_table, when collision
+void insert_hash_table(int value, int index, table *hash_table){
+    int hash_index = hash(value);
     for (int i = 0; i < TABLE_SIZE;i++){
-        int try = (i + index) % TABLE_SIZE;
-        if(hash_table[try]==EMPTY){
-            hash_table[try] = number;
+        int try = (hash_index + i) % TABLE_SIZE;
+        if(hash_table[try].value==EMPTY){
+            hash_table[try].value = value;
+            hash_table[try].index = index;
             return;
         }
     }
 }
 
-//if found the element, return index, else return -1
-int hash_table_lookup(int number, int *hash_table){
-    int index = hash(number);
+int hash_table_search(int value, table *hash_table){
+    int hash_index = hash(value);
     for (int i = 0; i < TABLE_SIZE;i++){
-        int try = (i + index) % TABLE_SIZE;
-        if(hash_table[try]==EMPTY)
+        int try = (hash_index + i) % TABLE_SIZE;
+        if(hash_table[try].value==EMPTY){
             return -1;
-        if(hash_table[try]==number)
-            return try;
+        }
+        else if(hash_table[try].value==value){
+            return hash_table[try].index;
+        }
     }
     return -1;
 }
 
-void print_hash_table(int *hash_table){
-    for (int i = 0; i < TABLE_SIZE;i++){
-        if(hash_table[i]!=EMPTY){
-            printf("index = %d, value = %d\n", i, hash_table[i]);
-        }
-    }
-}
-
-
 int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+    table hash_table[TABLE_SIZE];
+    init_hash_table(hash_table);
     *returnSize = 2;
     int *result = malloc(sizeof(int) * (*returnSize));
-    
-    init_hash_table(hash_table);
+
     for (int i = 0; i < numsSize;i++){
-        hash_table_insert(nums[i], hash_table);
+        insert_hash_table(nums[i], i, hash_table);
     }
 
-    int index;
     int i, j;
-    for (i = 0; i < numsSize;i++){
-        index = hash_table_lookup(target - nums[i], hash_table);
-        if(index!=-1){
-            result[0] = i;
-            for (j = 0; j < numsSize;j++){
-                if(j!=i && nums[j]==hash_table[index]){
-                    result[1] = j;
-                    break;
-                }
+    for (i = 0; i < numsSize; i++){
+        int compliment = target - nums[i];
+        int index = hash_table_search(compliment, hash_table);
+
+        //nums = [3,2,4], target = 6, avoid 3 + 3 condition 
+        if(nums[i]*2==target){
+            int flag = 0;
+            for (j = 0; j < numsSize && flag<2;j++){
+                if(nums[j]==nums[i])
+                    flag++;
             }
+            if(flag==2){
+                result[0] = i;
+                result[1] = j-1;
+                break;
+            }
+        }
+
+        else if(index!=-1){
+            result[0] = i;
+            result[1] = index;
             break;
         }
-        hash_table_insert(target - nums[i], hash_table);
     }
     return result;
 }
 
+void print_hash_table(table *hash_table){
+    for (int i = 0; i < TABLE_SIZE;i++){
+        if(hash_table[i].value!=EMPTY){
+            printf("hash index = %d, value = %d, index = %d\n", i, hash_table[i].value, hash_table[i].index);
+        }
+    }
+}
 
 int main(){
-    int nums[] = {3,2,4};
+    int nums[] = {2,7,11,15};
     int returnSize;
-    int target = 6;
+    int target = 9;
     int *result = twoSum(nums, sizeof(nums) / sizeof(int), target, &returnSize);
     printf("%d %d\n", result[0], result[1]);
     return 0;
